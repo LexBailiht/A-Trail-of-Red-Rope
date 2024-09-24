3c0e77035a0fee5983160115185ec7d19e885746
LexBailiht <lex.evan04@gmail.com>
2024-09-24 12:59:50 +0930

    build created

--- A trail of red rope/Assets/Managers/GameManager.cs
+++ A trail of red rope/Assets/Managers/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int DialogueNumber;
+    public int DialogueNumber = 0;
     // Start is called before the first frame update
     void Start()
     {
